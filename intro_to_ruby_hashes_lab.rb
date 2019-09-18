305e505418b5b9645c0a7feed44f99bc850fefe6
Huda Yousif <hjdoski@gmail.com>
2019-09-18 04:53:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,9 @@ def monopoly_with_fourth_tier
 		:railroads => {
 		  :names=>
 		  {
-  		 :reading_railroad => $100,
+  		 :reading_railroad => {
+  		   "mortgage_value"=>"$100"
+  		 },
   		 :pennsylvania_railroad=>{},
   		 :b_and_o_railroad=>{},
   		 :shortline_railroad=>{}
