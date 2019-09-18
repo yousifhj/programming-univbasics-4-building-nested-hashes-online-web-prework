693fed9dbdb6948f299467f588abcf0259e965ae
Huda Yousif <hjdoski@gmail.com>
2019-09-18 03:52:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,6 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+ {}
+ return
 end
 
 def monopoly_with_second_tier
