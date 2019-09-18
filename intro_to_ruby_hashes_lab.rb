96de016d9d4e2a0db052c40c011c9cc1a334a0c5
Huda Yousif <hjdoski@gmail.com>
2019-09-18 04:51:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_third_tier
   		 :reading_railroad=>{},
   		 :pennsylvania_railroad=>{},
   		 :b_and_o_railroad=>{},
-  		 :shortline=>{}
+  		 :shortline_railroad=>{}
 		  },
 	  :pieces => 4,
 	  :rent_in_dollars => 
